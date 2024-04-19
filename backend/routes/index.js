b2e50ff942013317d2093ac2fd9609ea89670d9f
Gino <ginogallo74@yahoo.com>
2024-04-19 16:43:15 -0400

    api endpoints

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const asyncHandler = require("express-async-handler");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {});
+router.get("/", asyncHandler(async (req, res, next) => {
+    res.status(200).json("GET index");
+  })
+);
 
-module.exports = router;
+module.exports = router; 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const authJWT = require("../config/passport").authJwt;
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {});
+// Controllers
+const user_controller = require("../controllers/usersController");
+
+// To Do
+// Create isAdmin
+
+// GET all users
+router.get("/details", authJWT, isAdmin, user_controller.user_list_get);
+
+// POST user Login.. Issue JWT Token
+router.post("/login", user_controller.user_login_post);
+
+// POST create a user
+router.post("/create", user_controller.user_create_post);
 
 module.exports = router;
