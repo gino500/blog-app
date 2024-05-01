155d71ed4a303bfb01a0dd990e57ee7e06b8c51e
Gino <ginogallo74@yahoo.com>
2024-05-01 14:18:35 -0400

    cleaned code

--- backend/routes/blogs.js
+++ backend/routes/blogs.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const authJWT = require("../config/passport").authJwt;
+const validator = require("../config/validator");
 
 // Controllers
 const blog_controller = require("../controllers/blogsController");
+const comment_controller = require("../controllers/commentController");
 
 // URI Blog Only
+
+// Create middleware to prevent user from accessing other user blogs
 
 // GET all blogs
 router.get("/", authJWT, blog_controller.blog_list_get);
 
 // POST create a blog
-router.post("/create", authJWT, blog_controller.blog_create_post);
+router.post("/create", authJWT, validator.blogCreateValidator, blog_controller.blog_create_post);
 
 // GET single blog
 router.get("/:id", authJWT, blog_controller.blog_detail_get);
 
 // PUT update a blog
-router.put("/update/:id", authJWT, blog_controller.blog_update_put);
+router.patch("/update/:id", authJWT, blog_controller.blog_update_patch);
 
 // DELETE a blog
 router.delete("/delete/:id", authJWT, blog_controller.blog_delete_post);
@@ -28,7 +32,7 @@ router.delete("/delete/:id", authJWT, blog_controller.blog_delete_post);
 router.get("/:blogid/comments", authJWT, comment_controller.comment_list_get);
 
 // POST create single comment for specific blog
-router.post("/:blogid/comments/create", authJWT, comment_controller.comment_create_post);
+router.post("/:blogid/comments/create", authJWT, validator.commentCreateValidator, comment_controller.comment_create_post);
 
 // GET single comment for specific blog
 router.get("/:blogid/comments/:commentid", authJWT, comment_controller.comment_detail_get);
@@ -37,6 +41,6 @@ router.get("/:blogid/comments/:commentid", authJWT, comment_controller.comment_d
 router.delete("/:blogid/comments/delete/:commentid", authJWT, comment_controller.comment_delete_post);
 
 // POST add Like to comment
-router.post("/:postid/comments/like/:commentid", authJWT, comment_controller.comment_like_post);
+router.post("/:blogid/comments/like/:commentid", authJWT, comment_controller.comment_like_post);
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authJWT = require("../config/passport").authJwt;
+const validator = require("../config/validator");
 
 // Controllers
 const user_controller = require("../controllers/usersController");
@@ -9,12 +10,12 @@ const user_controller = require("../controllers/usersController");
 // Create isAdmin
 
 // GET all users
-router.get("/details", authJWT, isAdmin, user_controller.user_list_get);
+router.get("/details", authJWT, user_controller.user_list_get);
 
 // POST user Login.. Issue JWT Token
-router.post("/login", user_controller.user_login_post);
+router.post("/login", validator.userLoginValidator, user_controller.user_login_post);
 
 // POST create a user
-router.post("/create", user_controller.user_create_post);
+router.post("/create", validator.userCreateValidator, user_controller.user_create_post);
 
 module.exports = router;
