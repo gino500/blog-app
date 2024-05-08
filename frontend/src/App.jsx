1405e2177bf2a9c711ca09f837298c8148f45f22
Gino <ginogallo74@yahoo.com>
2024-05-08 19:12:02 -0400

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import api from "./api/users";
+
+const App = () => {
+  return <div>Blogs</div>;
+};
+
+export default App;
