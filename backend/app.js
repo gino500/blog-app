b1effee99a3a55ffdc72e2c0ae3a6e46235afa32
Gino <ginogallo74@yahoo.com>
2024-07-19 20:09:13 -0400

    config

--- backend/app.js
+++ backend/app.js
@@ -55,8 +55,6 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
-
-  app.get("/", (req, res) => res.send("Server is Ready"));
 } else {
   app.get("/", (req, res) => res.send("Server is Ready"));
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,13 +10,13 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  // server: {
-  //   port: 3000,
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:5000",
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
+  server: {
+    port: process.env.PORT || 8080,
+    proxy: {
+      "/api": {
+        target: "blog-app-production-0294.up.railway.app",
+        changeOrigin: true,
+      },
+    },
+  },
 });
